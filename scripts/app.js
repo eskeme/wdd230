e8ea95e72d6d42eecbd2a2fd68b1775e3c1fe60a
eskeme <hanibelle.justine@gmail.com>
2023-01-07 14:41:18 +0800

    Update for correct assignment.

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,5 @@
-const h1 = document.querySelector('h1');
 const copyright = document.querySelector('#copyright');
 
-h1.textContent = 'Justine Insong';
-
 copyright.textContent = `© ${new Date().getFullYear()}`;
-let quantity = document.querySelector('#q').value;
 
-alert(document.lastModified);
+let oLastModif = document.getElementById("lastupdate").innerText = document.lastModified;
