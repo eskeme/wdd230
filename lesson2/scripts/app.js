63ae1bd3740c1957a32791cc895c89214d7b34f0
eskeme <hanibelle.justine@gmail.com>
2023-01-20 14:12:37 +0800

    Made changes to separate projects.

---
+++ lesson2/scripts/app.js
@@ -0,0 +1,5 @@
+const copyright = document.querySelector('#copyright');
+
+copyright.textContent = `© ${new Date().getFullYear()}`;
+
+let oLastModif = document.getElementById("lastupdate").innerText = document.lastModified;
