cc29c5d4a4943fc20da7cbd202500714cd3d36e5
eskeme <hanibelle.justine@gmail.com>
2023-01-27 13:17:33 +0800

    fixed all grids

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -12,8 +12,12 @@ let day_week = weekday[date.getDay()]
 document.getElementById("current_date").innerText = `${day_week}, ${day} ${month} ${year}`;
 
 copyright.textContent = `© ${new Date().getFullYear()}`;
+copyright1.textContent = `© ${new Date().getFullYear()}`;
+copyright2.textContent = `© ${new Date().getFullYear()}`;
 
 let oLastModif = document.getElementById("lastupdate").innerText = document.lastModified;
+let oLastModif1 = document.getElementById("lastupdate1").innerText = document.lastModified;
+let oLastModif2 = document.getElementById("lastupdate2").innerText = document.lastModified;
 
 function toggleMenu() {
     document.getElementById('primary-nav').classList.toggle('open');
