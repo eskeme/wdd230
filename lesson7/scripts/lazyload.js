92c48bb41d14421d2abb3541656fc88b3897d34b
eskeme <hanibelle.justine@gmail.com>
2023-02-14 20:21:54 +0800

    finally made the code to work.

--- lesson7/scripts/lazyload.js
+++ lesson7/scripts/lazyload.js
@@ -1,30 +1,32 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
+const dataSrc = image.getAttribute("data-src")
+if (!dataSrc) return
+  image.setAttribute("src", dataSrc);
   image.onload = () => {
     image.removeAttribute("data-src");
   };
 };
 
 imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
     loadImages(img);
   });
-
-  if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if (item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+}
