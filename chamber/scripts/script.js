d3201052ca1b35e7be89614c08673760406fc49c
eskeme <hanibelle.justine@gmail.com>
2023-02-20 04:14:33 +0800

    updated javascript

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -32,36 +32,41 @@ function toggleMenu() {
 const x = document.getElementById('hamburger-button')
 x.onclick = toggleMenu;
 
+const timeout = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+const now = new Date();
+
 if (localStorage.getItem('lastVisit')) {
-  // get the date of the last site visit from local storage
   const lastVisit = new Date(localStorage.getItem('lastVisit'));
-  
-  // calculate the time difference between today and the last site visit
-  const diffInMilliseconds = Math.abs(date - lastVisit);
-  const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
-  const diffInMinutes = Math.floor(diffInSeconds / 60);
-  const diffInHours = Math.floor(diffInMinutes / 60);
-  const diffInDays = Math.ceil(diffInHours / 24);
 
-  // display the appropriate message based on the time difference
-  const resultElement = document.getElementById('visits');
-  if (diffInDays === 1) {
-    resultElement.innerHTML = `Welcome back! It has been 1 day since you last visited.`;
-  } else if (diffInDays > 1) {
-    resultElement.innerHTML = `Welcome back! It has been ${diffInDays} days since you last visited.`;
+  if (now - lastVisit >= timeout) {
+    // Reset the local storage if the timeout has elapsed
+    localStorage.removeItem('lastVisit');
+    document.getElementById('visits').textContent = 'Welcome to the chamber website!';
   } else {
-    resultElement.innerHTML = 'Welcome to the chamber website!';
-  }
-  
-  // display the time difference in the console
-  console.log(`The time difference between today and the last site visit is:`);
-  console.log(`${diffInMilliseconds} milliseconds`);
-  console.log(`${diffInSeconds} seconds`);
-  console.log(`${diffInMinutes} minutes`);
-  console.log(`${diffInHours} hours`);
-  console.log(`${diffInDays} days`);
+    // Calculate and display the time difference if the timeout hasn't elapsed
+    const diffInMilliseconds = now - lastVisit;
+    const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    const diffInDays = Math.ceil(diffInHours / 24);
 
+    const resultElement = document.getElementById('visits');
+    if (diffInDays === 1) {
+      resultElement.innerHTML = `Welcome back! It has been 1 day since you last visited.`;
+    } else if (diffInDays > 1) {
+      resultElement.innerHTML = `Welcome back! It has been ${diffInDays} days since you last visited.`;
+    } else {
+      resultElement.innerHTML = 'Welcome to the chamber website!';
+    }
+
+    console.log(`The time difference between today and the last site visit is:`);
+    console.log(`${diffInMilliseconds} milliseconds`);
+    console.log(`${diffInSeconds} seconds`);
+    console.log(`${diffInMinutes} minutes`);
+    console.log(`${diffInHours} hours`);
+    console.log(`${diffInDays} days`);
+  }
 } else {
-  localStorage.setItem('lastVisit', date);
+  localStorage.setItem('lastVisit', now);
   document.getElementById('visits').textContent = 'Welcome to the chamber website!';
 }
